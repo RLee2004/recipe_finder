4152797c98c5cf23a844713bb47ecb73cee4cb02
Raymond Lee <2004raymondlee@gmail.com>
2025-09-01 19:46:47 -0700

    Added fetch call for recipe information

--- Recipe Finder/src/Components/RecipeCard.jsx
+++ Recipe Finder/src/Components/RecipeCard.jsx
@@ -1,12 +1,34 @@
-export default function RecipeCard({children, showDetails, onClose}) {
-    if (!showDetails) return null;
+import { useEffect, useState } from "react";
+const API_KEY = import.meta.env.VITE_MY_API_KEY;
+
+export default function RecipeCard({children, showDetails, onClose, id}) {
+    const [recipeDetails, setRecipeDetails] = useState({});
     
+    useEffect(() => {
+        const fetchRecipeDetails = async () => {
+            try {
+                const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+                const data = await response.json();
+                console.log(data);
+                setRecipeDetails(data);
+            }
+            catch (error) {
+                console.error('Error fetching recipe details:', error);
+            }
+        }
+
+        fetchRecipeDetails();
+    }, [showDetails]);
+
+    if (!showDetails) return null;
+
     return (
         <div className = "recipe-card"
             onClick = {onClose}>
             <div className="recipe-content"
                 onClick = {e => e.stopPropagation()}>
                 {children}
+                {recipeDetails.instructions}
             </div>
         </div>
     );
--- Recipe Finder/src/Components/RecipeInformation.jsx
+++ Recipe Finder/src/Components/RecipeInformation.jsx
@@ -3,12 +3,12 @@ import RecipeCard from './RecipeCard';
 
 export default function RecipeInformation({ recipe }) {
     const [showDetails, setShowDetails] = useState(false);
-
+    
     return (<div className = "recipe-information">
         <img src={recipe.image} alt={recipe.title} />
         <h3> {recipe.title}</h3>
         <button onClick = {() => {setShowDetails(true)}}>View Recipe</button>
-        <RecipeCard showDetails = {showDetails} onClose = {() => setShowDetails(false)}>
+        <RecipeCard showDetails = {showDetails} onClose = {() => setShowDetails(false)} id = {recipe.id}>
             <img src = {recipe.image}></img>
             <h1>{recipe.title}</h1>
             Missing Ingredients: {recipe.missedIngredients.map(ing => ing.name).join(', ')} <br/>
